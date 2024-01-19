008d7af73631924db6ffffd9e884df8ab7c2cce5
Daniel Souza <danielbasouza@gmail.com>
2024-01-18 21:33:42 -0300

    Add Progress Bar and loading cars list in car fragment

--- app/src/main/java/br/com/danielsouza/eletriccarapp/ui/CarFragment.kt
+++ app/src/main/java/br/com/danielsouza/eletriccarapp/ui/CarFragment.kt
@@ -1,6 +1,7 @@
 package br.com.danielsouza.eletriccarapp.ui
 
 import android.content.Intent
+import android.net.http.HttpResponseCache
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import br.com.danielsouza.eletriccarapp.R
@@ -20,13 +22,15 @@ import org.json.JSONTokener
 import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
 
 class CarFragment : Fragment() {
     lateinit var fabCalcular: FloatingActionButton
     lateinit var listaCarros: RecyclerView
+    lateinit var progressBar: ProgressBar
 
-    var carrosArray : ArrayList<Carro> = ArrayList()
+    var carrosArray: ArrayList<Carro> = ArrayList()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -38,6 +42,7 @@ class CarFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupView(view)
+        callServices()
         setupListeners()
         setupList()
     }
@@ -46,27 +51,38 @@ class CarFragment : Fragment() {
         view?.apply {
             fabCalcular = findViewById(R.id.fab_calcular)
             listaCarros = findViewById(R.id.rv_lista_carros)
+            progressBar = findViewById(R.id.pb_loader)
         }
 
     }
 
     fun setupList() {
-        val adapter = CarAdapter(carrosArray)
+        val adapterList = CarAdapter(carrosArray)
 
-        listaCarros.adapter = adapter
+        listaCarros.apply{
+            visibility = View.VISIBLE
+            adapter = adapterList
+        }
+
     }
 
     fun setupListeners() {
         fabCalcular.setOnClickListener {
-            MyTask().execute("https://igorbag.github.io/cars-api/cars.json")
-//            startActivity(Intent(context, CalcularAutonomiaActivity::class.java))
+            startActivity(Intent(context, CalcularAutonomiaActivity::class.java))
         }
+    }
+
+    fun callServices() {
+        val urlBase = "https://igorbag.github.io/cars-api/cars.json"
+        MyTask().execute(urlBase)
+
     }
 
     inner class MyTask : AsyncTask<String, String, String>() {
         override fun onPreExecute() {
             super.onPreExecute()
             Log.d("MyTask", "Iniciando...")
+            progressBar.visibility = View.VISIBLE
         }
 
         override fun doInBackground(vararg url: String?): String {
@@ -78,10 +94,19 @@ class CarFragment : Fragment() {
                 urlConnection = urlBase.openConnection() as HttpURLConnection
                 urlConnection.connectTimeout = 60000
                 urlConnection.readTimeout = 60000
+                urlConnection.setRequestProperty(
+                    "Accept",
+                    "application/json"
+                )
 
-                var response = urlConnection.inputStream.bufferedReader().use { it.readText() }
-                publishProgress(response)
+                val responseCode = urlConnection.responseCode
 
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    var response = urlConnection.inputStream.bufferedReader().use { it.readText() }
+                    publishProgress(response)
+                } else {
+                    Log.e("Erro", "Erro ao realizar processamento...")
+                }
             } catch (ex: Exception) {
                 Log.e("Erro", "Erro ao realizar processamento...")
             } finally {
@@ -117,7 +142,6 @@ class CarFragment : Fragment() {
                     Log.d("Model ->", model.toString())
                 }
                 setupList()
-
             } catch (ex: Exception) {
                 Log.e("Erro ->", ex.message.toString())
             }
